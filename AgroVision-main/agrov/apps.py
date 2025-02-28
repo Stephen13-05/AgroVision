3357626ee5486fac0529e9819e2ef9a1349d8ef1
Asrithrajeshv <144985321+Asrithrajeshv@users.noreply.github.com>
2025-02-28 14:07:12 +0530

    Delete AgroVision-main directory

--- AgroVision-main/agrov/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgrovConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agrov'
--- AgroVision-main/weather/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'weather'
-
-urlpatterns = [
-    path('', views.weather_dashboard, name='dashboard'),
-    path('data/', views.get_weather_data, name='get_weather_data'),
-] 
