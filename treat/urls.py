41be2151f608ec6daba11457e0bc5232408b34a4
Asrithrajeshv <230068.cb@rmkec.ac.in>
2025-03-01 00:28:13 +0530

    abc

--- treat/urls.py
+++ treat/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('pestinput/', views.pest_input, name='pest_input'),  # URL for pest input
     path('get-treatment-pest/<str:pest_name>/', views.get_treatment_pest, name='get_treatment_pest'),  # URL for getting treatment for a pest
     path('pestdf/', views.pest_pdf, name='pest_pdf'),  # URL for downloading pest PDF
+    path('forecast-recommendations/', views.generate_growth_recommendations, name='forecast_recommendations'),  # URL for weather forecast and growth recommendations
+
 ]
