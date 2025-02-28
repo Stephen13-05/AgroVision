2fea48d77fd231759d61672ba0c13ec3ed7d8026
sandeep <100069895+30sandz@users.noreply.github.com>
2025-03-01 03:55:55 +0530

    nothing

--- agrov/model_loader.py
+++ agrov/model_loader.py
@@ -6,8 +6,8 @@ import os
 
 # Define paths for both models
 MODEL_PATHS = {
-    "pes": r"C:/ai/data preprocessing/mobilenetv2_model.h5",
-    "diease": r"C:/ai/ai/mobilenetv2_model.h5",
+    "pest": r"C:/ai/data preprocessing/mobilenetv2_model.h5",
+    "disease": r"C:/ai/ai/mobilenetv2_model.h5",
 }
 
 # Load models
